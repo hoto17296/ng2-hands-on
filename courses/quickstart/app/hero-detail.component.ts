7e494fe6a4c8b21e0a40b00dea17d7edce59488c
石川 雄基 / ほと <yuuki.ishikawa@gaiax.com>
2016-07-25 10:13:45 +0900

    app

---
+++ courses/quickstart/app/hero-detail.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from './hero.service';
+import { Hero } from './hero';
+
+@Component({
+  selector: 'my-hero-detail',
+  templateUrl: 'app/hero-detail.component.html',
+})
+export class HeroDetailComponent {
+  hero: Hero;
+  sub: any; // rxjs.Subscription
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute) {
+  }
+  ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      let id = +params['id'];
+      this.heroService.getHero(id).then(hero => this.hero = hero);
+    });
+  }
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+  goBack() {
+    window.history.back();
+  }
+}
